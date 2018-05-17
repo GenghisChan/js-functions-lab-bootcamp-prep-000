3ea32da27a00b0d7005830d82d7ad6128f9d069e
Christofer Chan <devchumbing@gmail.com>
2018-05-17 09:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,5 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name]!`
-}
-
-function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
-  
+  return "Happy holidays " + ${name] + ' !'
 }
